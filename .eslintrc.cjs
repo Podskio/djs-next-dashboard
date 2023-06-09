e16e51b3f0412102162763c5892f8aab53358334
Podskio <podskio1@gmail.com>
2023-06-08 23:41:41 -0700

    Eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["prettier", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: "apps/web/**",
+      plugins: ["react", "react-hooks"],
+      extends: [
+        "next",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+      ],
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+    },
+  ],
+  settings: {
+    next: {
+      rootDir: "apps/web",
+    },
+  },
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    "*.config.cjs",
+    "**/dist/**",
+    "**/.next/**",
+  ],
+  root: true,
+};
