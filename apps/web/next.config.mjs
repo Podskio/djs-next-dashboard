90d1b65a18ea71f51592e3fb55fb71d624453fb0
Podskio <podskio1@gmail.com>
2023-06-08 17:08:10 -0700

    ct3a

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,12 @@
+await import("./src/env.mjs");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;
