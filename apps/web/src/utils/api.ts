4a067c95424ebb2f7d6023d5ee12e710345fdb16
Podskio <podskio1@gmail.com>
2023-06-19 17:02:27 -0700

    Export getBaseUrl

--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -10,7 +10,7 @@ import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 import { type AppRouter } from "~/server/api/root";
 
-const getBaseUrl = () => {
+export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
